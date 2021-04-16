da1475ead825d56a8c65d95264fdf3f7ebebeb60
Neryx33 <nerysthamm@gmail.com>
2021-04-16 17:50:14 +1200

    Added mouse control

---
+++ XRTS_TEMPLATE/Assets/Scripts/ManualControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Motor))]
+public class ManualControl : MonoBehaviour
+{
+    Collider2D m_collider;
+    Motor m_motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collider = GetComponent<Collider2D>();
+        m_motor = GetComponent<Motor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(2))
+        {
+            Vector2 mousedirection = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position);
+            RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, mousedirection, mousedirection.magnitude);
+            if(!(hit.collider != null && hit.collider.CompareTag("Wall")))
+            {
+                m_motor.m_direction += mousedirection.normalized;
+            }
+            else
+            {
+                m_motor.m_direction = Vector2.zero;
+            }
+
+        }
+    }
+}
